0f889d95f7fac7aa5d9b443960eb2bff9e838b84
Sebastian Sauerer <sebastian.sauerer@sap.com>
2022-05-16 18:20:43 +0200

    love

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
-import { Page, Text } from '@geist-ui/core'
+import { Link, Page, Text } from '@geist-ui/core'
 import type { NextPage } from 'next'
 import { Upload } from '../components/upload.component'
 
 const Home: NextPage = () => {
   return (
-    <Page id='dropzone'>
+    <Page id='dropzone' style={{ minHeight: '90vh' }}>
       <Page.Header center mt={5} style={{ display: 'flex', flexDirection: 'column', textAlign: 'center' }}>
         <Text h1 font={'4rem'} margin={0}>SVG Optimize</Text>
         <Text p font={'1rem'} style={{ color: '#696969' }}>
@@ -14,6 +14,9 @@ const Home: NextPage = () => {
       <Page.Content style={{ display: 'flex', placeContent: 'center' }}>
         <Upload />
       </Page.Content>
+      <Page.Footer style={{ display: 'flex', placeContent: 'center' }}>
+        <Text p>Built with ❤️ by <Link href="https://www.sause.dev/" icon color target={'_blank'}>Boring Contributor</Link></Text>
+      </Page.Footer>
     </Page >
   )
 }
